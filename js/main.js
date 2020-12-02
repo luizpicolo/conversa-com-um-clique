b087fe0d476037a2853d3cbf584017fbd5e7b26d
Luiz Picolo <luizpicolo@gmail.com>
2020-12-02 11:17:42 -0400

    Commit inicial

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$('#phone').mask('(00) 0000-0000');
+
+$( "#target" ).submit(function( event ) {
+  event.preventDefault();
+  let phone = $("#phone").val();
+  if (phone != ''){
+    $( "section" ).removeClass( "display-none" );
+    phone = phone.replace(/[^\d]/g, ''); 
+    $('.link-wa').html('<a href="https://wa.me/55'+phone+'">https://wa.me/55'+phone+'</a>');
+  } else {
+    alert("É necessário o número telefônico")
+  }
+});
