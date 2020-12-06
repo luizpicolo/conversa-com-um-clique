914a98ec6c3042e4953f80c4961a91d50d8cf58d
Luiz Picolo <luizpicolo@gmail.com>
2020-12-06 19:24:31 -0400

    Adicionada opção para abrir ou gerar link

--- js/main.js
+++ js/main.js
@@ -3,10 +3,20 @@ $('#phone').mask('(00) 00000-0000');
 $( "#target" ).submit(function( event ) {
   event.preventDefault();
   let phone = $("#phone").val();
+  let typeSubmition = $('input[name="type-submition"]:checked').val();
+
   if (phone != ''){
-    $( "section" ).removeClass( "display-none" );
     phone = phone.replace(/[^\d]/g, ''); 
-    $('.link-wa').html('<a href="https://wa.me/55'+phone+'">https://wa.me/55'+phone+'</a>');
+    let uri = `https://wa.me/55${phone}`;
+    let anchor = `<a href="https://wa.me/55${phone}">${uri}</a>`;
+
+    if (typeSubmition == 0){
+      $( "section" ).removeClass( "display-none" );
+      
+      $('.link-wa').html(anchor);
+    } else {
+      window.location.href = uri
+    }
   } else {
     swal("Vixii, aconteceu um problema", "Você precisa adicionar um número");
   }
